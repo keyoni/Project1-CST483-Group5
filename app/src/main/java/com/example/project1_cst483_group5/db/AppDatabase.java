5380bccf97e8af86f92f935604749daaec85c3d3
Michael <mhaidar@csumbn.edu>
2021-08-31 11:46:10 -0700

    Began implementing Pet entity for Room. Check comments in code for structure. TODO: implement DAO for pet; https://developer.android.com/codelabs/android-room-with-a-view#5

--- app/src/main/java/com/example/project1_cst483_group5/db/AppDatabase.java
+++ app/src/main/java/com/example/project1_cst483_group5/db/AppDatabase.java
@@ -1,4 +1,15 @@
 package com.example.project1_cst483_group5.db;
 
-public abstract class AppDatabase {
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {User.class, Pet.class}, version = 1)
+
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract UserDao userDao();
+    public abstract PetDao petDao();
+    
 }
+
+
+
--- app/src/main/java/com/example/project1_cst483_group5/db/Pet.java
+++ app/src/main/java/com/example/project1_cst483_group5/db/Pet.java
@@ -1,4 +1,101 @@
 package com.example.project1_cst483_group5.db;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+
+//pet table should have
+//petID(primary key), userID(foreign key), name, type, gender, id
+@Entity(tableName = "pet_table")
 public class Pet {
+
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    @ColumnInfo(name = "petID")
+    private final Integer mPetID;
+
+    //@ForeignKey()
+    @NonNull
+    @ColumnInfo(name = "userID")
+    private Integer mUserID;
+
+    @NonNull
+    @ColumnInfo(name = "name")
+    private String mName;
+
+    @NonNull
+    @ColumnInfo(name = "type")
+    private String mType;
+
+    @NonNull
+    @ColumnInfo(name = "gender")
+    private String mGender;
+
+    @NonNull
+    @ColumnInfo(name = "id")
+    private Integer mID;
+
+
+    public Pet(@NonNull Integer petID, @NonNull Integer mUserID, @NonNull String mName, @NonNull String mType, @NonNull String mGender, @NonNull Integer mID) {this.mPetID = petID;
+        this.mUserID = mUserID;
+        this.mName = mName;
+        this.mType = mType;
+        this.mGender = mGender;
+        this.mID = mID;
+    }
+
+    public Integer getPet(){return this.mPetID;}
+
+
+    @NonNull
+    public Integer getmID() {
+        return mID;
+    }
+
+    public void setmID(@NonNull Integer mID) {
+        this.mID = mID;
+    }
+
+    @NonNull
+    public Integer getmPetID() {
+        return mPetID;
+    }
+
+    @NonNull
+    public Integer getmUserID() {
+        return mUserID;
+    }
+
+    public void setmUserID(@NonNull Integer mUserID) {
+        this.mUserID = mUserID;
+    }
+
+    @NonNull
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(@NonNull String mName) {
+        this.mName = mName;
+    }
+
+    @NonNull
+    public String getmType() {
+        return mType;
+    }
+
+    public void setmType(@NonNull String mType) {
+        this.mType = mType;
+    }
+
+    @NonNull
+    public String getmGender() {
+        return mGender;
+    }
+
+    public void setmGender(@NonNull String mGender) {
+        this.mGender = mGender;
+    }
 }

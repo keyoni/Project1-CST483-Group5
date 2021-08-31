a456de10ef2a75777d385fd45270a90ef9870e53
Alexis Sanchez <alexisanchez@csumb.edu>
2021-08-31 11:42:32 -0700

    Adding settings page...

---
+++ app/src/main/java/com/example/project1_cst483_group5/Settings.java
@@ -0,0 +1,15 @@
+package com.example.project1_cst483_group5;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+    }
+}

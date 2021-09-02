ccd24c50556c0339bc4abc24ae40bc90dc0e67c7
keyoni <56132773+keyoni@users.noreply.github.com>
2021-09-02 12:04:41 -0700

    Revert "designing the layouts for CAA and search pages;"
    This reverts commit 9e258b851058da6ce5ecb9526c9c4456b28c71b2.

--- app/src/main/java/com/example/project1_cst483_group5/MainActivity.java
+++ app/src/main/java/com/example/project1_cst483_group5/MainActivity.java
@@ -2,27 +2,13 @@ package com.example.project1_cst483_group5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //set up a click listener for the Create Account button on the welcome(home) page.
-        View createAccountButton = findViewById(R.id.btnCreateAccount);
-        createAccountButton.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-        if(view.getId() == R.id.btnCreateAccount){
-            Intent i = new Intent(this, CreateAnAccount.class);
-            startActivity(i);
-        }
     }
 }

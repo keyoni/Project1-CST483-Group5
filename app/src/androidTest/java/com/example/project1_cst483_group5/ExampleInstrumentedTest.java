fddb1472acd09dd6a4c9d2c1b0d4733790c460a6
Michael <mhaidar@csumbn.edu>
2021-09-02 11:55:32 -0700

    Added Retrofit api and added retrofit test for api call. Test failed but Test did run so thats nice. Todo: finish API and pass test https://developer.android.com/codelabs/android-room-with-a-view#5 https://www.petfinder.com/developers/v2/docs/

--- app/src/androidTest/java/com/example/project1_cst483_group5/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/project1_cst483_group5/ExampleInstrumentedTest.java
@@ -1,26 +1,26 @@
 package com.example.project1_cst483_group5;
 
 import android.content.Context;
+import android.os.Bundle;
+import android.widget.TextView;
 
-import androidx.room.Room;
-import androidx.test.core.app.ApplicationProvider;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import com.example.project1_cst483_group5.db.AppDatabase;
-import com.example.project1_cst483_group5.db.User;
-import com.example.project1_cst483_group5.db.UserDao;
-
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.io.IOException;
 import java.util.List;
 
-import static org.hamcrest.Matchers.equalTo;
+
 import static org.junit.Assert.*;
+
+import Retro.Post;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -35,30 +35,40 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.project1_cst483_group5", appContext.getPackageName());
     }
+    @Before
+    protected void onCreate(Bundle savedInstanceState) {
+        TextView textViewResult = null;
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.petfinder.com/v2/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
 
-    @RunWith(AndroidJUnit4.class)
-    public class SimpleEntityReadWriteTest {
-        private UserDao userDao;
-        private AppDatabase db;
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if (!response.isSuccessful()) {
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+                List<Post> posts = response.body();
+                for (Post post : posts) {
+                    String content = "";
+                    content += "ID: " + post.getId() + "\n";
+                    content += "Type: " + post.getType() + "\n";
+                    content += "Gender: " + post.getGender() + "\n";
+                    content += "Age: " + post.getAge() + "\n\n";
+                    textViewResult.append(content);
+                }
+            }
 
-        @Before
-        public void createDb() {
-            Context context = ApplicationProvider.getApplicationContext();
-            db = Room.inMemoryDatabaseBuilder(context, AppDatabase.class).build();
-            userDao = db.userDao();
-        }
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
 
-        @After
-        public void closeDb() throws IOException {
-            db.close();
-        }
 
-        @Test
-        public void writeUserAndReadInList() throws Exception {
-            User user = new User("bob","bobby123","12345");
-            userDao.insert(user);
-            String byName = userDao.getName(1);
-            assertEquals(byName, "bob");
-        }
+        });
     }
 }

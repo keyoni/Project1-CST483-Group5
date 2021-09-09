8da160cd901750bfba633104b2b50530ebaf112f
Michael <mhaidar@csumbn.edu>
2021-09-09 13:52:16 -0700

    AndimalResults and PetFinderApi

---
+++ app/src/main/java/Retro/Post.java
@@ -0,0 +1,35 @@
+package Retro;
+
+public class Post {
+
+    private int userId;
+    private int id;
+    private String type;
+    private String gender;
+    private String age;
+    private String name;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}

0df4939cdcaa7302fbcce23b94cd3d1cc85f4f26
keyoni <56132773+keyoni@users.noreply.github.com>
2021-09-09 09:27:17 -0700

    adding Animal start

---
+++ app/src/main/java/com/example/project1_cst483_group5/Animal.java
@@ -0,0 +1,34 @@
+package com.example.project1_cst483_group5;
+
+import androidx.room.ColumnInfo;
+
+public class Animal {
+    //ToDO: Fill out with params we want from api
+
+    @ColumnInfo(name = "type")
+    String mType;
+
+    @ColumnInfo(name = "breed")
+    String mBreed;
+
+    @ColumnInfo(name = "size")
+    String mSize;
+
+    @ColumnInfo(name = "gender")
+    String mGender;
+
+    @ColumnInfo(name = "age")
+    String mAge;
+
+    @ColumnInfo(name = "color")
+    String mColor;
+
+    @ColumnInfo(name = "name")
+    String mName;
+
+    @ColumnInfo(name = "description")
+    String mDescription;
+
+
+
+}

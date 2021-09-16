2e29d10547691ee7ff0d9d985772afdf18574992
Alexis Sanchez <alexisanchez@csumb.edu>
2021-09-16 03:00:53 -0700

    Working on creating an account still

--- app/src/main/java/com/example/project1_cst483_group5/CreateAnAccount.java
+++ app/src/main/java/com/example/project1_cst483_group5/CreateAnAccount.java
@@ -3,9 +3,11 @@ package com.example.project1_cst483_group5;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
@@ -27,15 +29,22 @@ public class CreateAnAccount extends AppCompatActivity {
             public void onClick(View v) {
                 EditText cname, cpassword, cemail;
                 String name, password, username;
-
+                boolean isValid = true;
                 cemail = findViewById(R.id.etLoginEmail);
                 cname = findViewById(R.id.etFirstName);
                 cpassword = findViewById(R.id.etCreatePassword);
                 name = cname.getText().toString();
                 password = cpassword.getText().toString();
                 username = cemail.getText().toString();
-                User newUser = new User(name, username, password);
-                userVM.insert(newUser);
+                if(username.isEmpty() || password.isEmpty() || name.isEmpty()) {
+                    isValid = false;
+                    String text = "Neither fields can be left blank. Please try again";
+                    Toast.makeText(CreateAnAccount.this, text, Toast.LENGTH_SHORT).show();
+                }
+                    User newUser = new User(name, username, password);
+                    userVM.insert(newUser);
+                    startActivity(new Intent(CreateAnAccount.this, Favorites.class));
+                    Log.d("testCreateAcc", "this is working");
             }
         });
     }
--- app/src/main/java/com/example/project1_cst483_group5/db/AppDatabase.java
+++ app/src/main/java/com/example/project1_cst483_group5/db/AppDatabase.java
@@ -25,7 +25,8 @@ public abstract class AppDatabase extends RoomDatabase {
             synchronized (AppDatabase.class) {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                            AppDatabase.class, "app_database")
+                            AppDatabase.class, "app_database").allowMainThreadQueries()
+                            .fallbackToDestructiveMigration()
                             .build();
                 }
             }
